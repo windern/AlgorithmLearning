cc3d2e38def123567b1b2305843806c277d7188b
windern <windern0712@126.com>
2019-08-25 10:09:49 +0800

    init

---
+++ src/com/windern/algorithmlearning/Main.java
@@ -0,0 +1,9 @@
+package com.windern.algorithmlearning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("hello world");
+    }
+}
