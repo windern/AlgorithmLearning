8244f980c6dd199f1064201c73645ac192419222
windern <windern0712@126.com>
2019-09-05 01:02:28 +0800

    实现Offer14剪绳子，贪婪算法的实现

--- src/com/windern/algorithmlearning/Offer/Offer14.java
+++ src/com/windern/algorithmlearning/Offer/Offer14.java
@@ -8,7 +8,7 @@ public class Offer14 {
      * @param n 数字
      * @return 结果
      */
-    public static int integerBreak(int n) {
+    public static int integerBreakWithDynamic(int n) {
         int[] dp = new int[n + 1];
         dp[1] = 1;
         for (int i = 4; i <= n; i++) {
@@ -21,7 +21,33 @@ public class Offer14 {
         return dp[n];
     }
 
+    /**
+     * 贪婪算法实现
+     *
+     * @param n 数字
+     * @return 结果
+     */
+    public static int integerBreakWithGreedy(int n) {
+        if (n < 2) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        } else if (n == 3) {
+            return 2;
+        }
+
+        // 很巧妙，计算3和2的方法
+        int timesOfThree = n / 3;
+        if (n - 3 * timesOfThree == 1) {
+            timesOfThree--;
+        }
+        // 减去3的总数后剩下给2
+        int timesOfTwo = (n - 3 * timesOfThree) / 2;
+        return (int) (Math.pow(3, timesOfThree) * Math.pow(2, timesOfTwo));
+    }
+
     public static void main(String[] args) {
-        System.out.println(integerBreak(5));
+        System.out.println(integerBreakWithDynamic(10));
+        System.out.println(integerBreakWithGreedy(10));
     }
 }
