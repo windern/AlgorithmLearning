3ca596bffdc60937364b35960a1e15fc6cc0ea0b
windern <windern0712@126.com>
2019-09-22 17:06:52 +0800

    29. 顺时针打印矩阵的实现

--- src/com/windern/algorithmlearning/Offer/Offer29.java
+++ src/com/windern/algorithmlearning/Offer/Offer29.java
@@ -15,7 +15,7 @@ public class Offer29 {
         int num = 1;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
-                matrix[row][column] = num++;
+                matrix[i][j] = num++;
             }
         }
         return matrix;
@@ -24,14 +24,44 @@ public class Offer29 {
     public static ArrayList<Integer> printMatrix(int[][] matrix) {
         ArrayList<Integer> list = new ArrayList<>();
 
-        int r = 0;
-        int c = matrix[0].length;
+        // r1起始行
+        int r1 = 0;
+        // r2截止行
+        int r2 = matrix.length - 1;
+        // c1起始列
+        int c1 = 0;
+        // c2截止列
+        int c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int i = c1; i <= c2; i++) {
+                list.add(matrix[r1][i]);
+            }
+            for (int i = r1 + 1; i <= r2; i++) {
+                list.add(matrix[i][c2]);
+            }
+            // 要排除r1和r2一样，否则多打印
+            if (r1 != r2) {
+                for (int i = c2 - 1; i >= c1; i--) {
+                    list.add(matrix[r2][i]);
+                }
+            }
+            // 要排除c1和c2一样，否则多打印
+            if (c1 != c2) {
+                for (int i = r2 - 1; i >= r1 + 1; i--) {
+                    list.add(matrix[i][c1]);
+                }
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
 
         return list;
     }
 
     public static void main(String[] args) {
-        int[][] matrix = createMatrix(4, 4);
+        int[][] matrix = createMatrix(3, 4);
 
         ArrayList<Integer> list = printMatrix(matrix);
         System.out.println(list);
