bdd66f8afa0b06a7fe9a418cbde363cde5e53fae
windern <windern0712@126.com>
2019-10-09 07:31:52 +0800

    归并排序使用哨兵简化代码

--- src/com/windern/algorithmlearning/GeekTimeWangZheng/g12_sort/MergeSort.java
+++ src/com/windern/algorithmlearning/GeekTimeWangZheng/g12_sort/MergeSort.java
@@ -20,7 +20,8 @@ public class MergeSort {
         int mid = l + (h - l) / 2;
         mergeSort(array, l, mid);
         mergeSort(array, mid + 1, h);
-        merge(array, l, mid, h);
+//        merge(array, l, mid, h);
+        mergeBySentry(array, l, mid, h);
     }
 
     public static void merge(int[] array, int l, int m, int h) {
@@ -43,6 +44,34 @@ public class MergeSort {
 
         for (int k = l; k <= h; k++) {
             array[k] = tmpArray[k];
+        }
+    }
+
+    public static void mergeBySentry(int[] array, int l, int m, int h) {
+        int[] leftArray = new int[m - l + 2];
+        int[] rightArray = new int[h - m + 1];
+
+        for (int i = 0; i <= m - l; i++) {
+            leftArray[i] = array[l + i];
+        }
+        // 第一个数组添加哨兵（最大值）
+        leftArray[m - l + 1] = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= h - m - 1; i++) {
+            rightArray[i] = array[m + 1 + i];
+        }
+        // 第二个数组添加哨兵（最大值）
+        rightArray[h - m] = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k <= h - l; k++) {
+            // 当左边数组到达哨兵值时，i不再增加，直到右边数组读取完剩余值，同理右边数组也一样
+            if (leftArray[i] <= rightArray[j]) {
+                array[l + k] = leftArray[i++];
+            } else {
+                array[l + k] = rightArray[j++];
+            }
         }
     }
 
