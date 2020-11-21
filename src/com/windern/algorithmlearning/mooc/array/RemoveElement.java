1d0596a60eefe5ef1c3ef5d6b77d0764afa76784
windern <wenxinlin@guazi.com>
2020-11-21 18:09:32 +0800

    移除元素

---
+++ src/com/windern/algorithmlearning/mooc/array/RemoveElement.java
@@ -0,0 +1,32 @@
+package com.windern.algorithmlearning.mooc.array;
+
+import java.util.Arrays;
+
+/**
+ * @author windern
+ * @date 2020/11/21 17:39
+ **/
+public class RemoveElement {
+
+    public static int removeElement(int[] nums, int val) {
+        // [0,k)存储非val的数据
+        // [k,nums)存储val的数据
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 5, 1};
+        int result = removeElement(nums, 1);
+        System.out.println("result: " + result);
+        System.out.println(Arrays.toString(nums));
+    }
+}
